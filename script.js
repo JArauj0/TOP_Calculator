3a1bab9c1dfeb44f8d9a85f1c3ca0bca9cd5269c
João Araújo <joaoaraujo.78@gmail.com>
2022-03-14 12:05:23 +0000

    first revision

---
+++ script.js
@@ -0,0 +1,11 @@
+function operation (a, b, operator) {
+    if (operator == '+') {
+        return a+b;
+    } else if (operator == '-') {
+        return a-b;
+    } else if (operator == '*') {
+        return a*b;
+    } else if (operator == '/') {
+        return a/b;
+    }
+}
